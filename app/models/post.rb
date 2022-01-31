55bb923398d14944def9c1fdea2e2e887bb99ac7
Erik Alvar <erikalvarbass@gmail.com>
2022-01-31 18:00:46 -0500

    writes tests for post and user and fixes fails by writing validators in models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, :body, presence: true, length: { minimum: 3 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:posts) }
+  it { should validate_presence_of(:username) }
+  it { should validate_length_of(:username).is_at_least(3) }
 end

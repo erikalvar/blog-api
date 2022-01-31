486015037c7c5eb2060d9c5eb685ba9d441236e5
Erik Alvar <erikalvarbass@gmail.com>
2022-01-31 17:46:59 -0500

    creates User and Post models and assoctiations

---
+++ db/migrate/20220131223656_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :location
+      t.date :birthdate
+      t.boolean :admin
+      t.string :first_name
+      t.string :last_name
+
+      t.timestamps
+    end
+  end
+end
